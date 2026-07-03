731ec2f127e5086fa51307f2a65dd0517d8b3d5a
naitiksaxena06-design <naitiksaxena06@gmail.com>
2026-07-03 05:52:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1801,6 +1801,7 @@ function WhiteboardChat({ chats, userProfile, chatChannel, setChatChannel, pushN
       )}      
     );
   }
+  }             
 // --- INSTA FEED ---
 function PostsWorkspace({ posts, userProfile, showToast, pushNotification, isAdmin, onInspectUser }) {
   const [postTitle, setPostTitle] = useState('');
